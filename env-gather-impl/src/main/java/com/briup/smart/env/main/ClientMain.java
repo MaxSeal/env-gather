70653e14c22934217a85ebcda97ed3c0f5cd584f
MaxSeal <1204850179@qq.com>
2021-07-27 11:44:00 +0800

    完成了 ClientMain（客户端的入口方法）

--- env-gather-impl/src/main/java/com/briup/smart/env/main/ClientMain.java
+++ env-gather-impl/src/main/java/com/briup/smart/env/main/ClientMain.java
@@ -1,8 +1,24 @@
 package com.briup.smart.env.main;
 
+import com.briup.smart.env.client.Client;
+import com.briup.smart.env.client.ClientImpl;
+import com.briup.smart.env.client.Gather;
+import com.briup.smart.env.client.GatherImpl;
+import com.briup.smart.env.entity.Environment;
+import java.util.Collection;
+
 //客户端入口类
 public class ClientMain {
 	public static void main(String[] args) {
 
+		Gather gather = new GatherImpl();
+		Client client = new ClientImpl();
+
+		try {
+			Collection<Environment> c = gather.gather();
+			client.send(c);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
